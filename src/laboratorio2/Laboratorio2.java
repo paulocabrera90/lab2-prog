ba7bea6a59c83ef83e549b67a73436bae23771d3
Martin <martinzanche@gmail.com>
2019-09-09 15:37:23 -0300

    Agragado parte Martin ej del smatphone.

--- src/laboratorio2/Laboratorio2.java
+++ src/laboratorio2/Laboratorio2.java
@@ -4,9 +4,12 @@
  * and open the template in the editor.
  */
 package laboratorio2;
+import java.util.Scanner;
 
+import objects.Celular;
 import objects.Deportista;
 import objects.Persona;
+import objects.SmartPhone;
 
 /**
  *
@@ -26,6 +29,37 @@ public class Laboratorio2 {
         Deportista d1=new Deportista("juan",32,21454634,"barrio 2",23);
         d1.anDar();
         d1.coRrer();
+        
+        //Parte Martin
+        //Ejecucion celular
+        System.out.println("");
+        System.out.println("");
+        Celular c1= new Celular();
+        c1.setColor("Gris");
+        c1.setMarca("Motorola");
+        c1.setModelo("1100");
+        c1.llamar("Juan");
+        c1.finalizarLlamada();
+        c1.informarCaracteristicas();
+        
+        //Ejecucion celular
+        System.out.println("");
+        System.out.println("");
+        SmartPhone smart2= new SmartPhone();
+        smart2.setColor("Blanco");
+        smart2.setMarca("Samsung");
+        smart2.setModelo("J9");
+        smart2.setPixelsCamara("35mpx");
+        smart2.setTamañoMemoriaExterna("16gb");
+        smart2.setTamañoMemoriaInterna("8GB");
+        smart2.llamar("Valeria Lynch");
+        smart2.finalizarLlamada();
+        smart2.informarCaracteristicas();
+        
+       
+        
+       
     }
+    
     
 }
